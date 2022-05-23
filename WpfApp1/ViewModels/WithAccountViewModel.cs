d99a80152059684c1470696b9f8a16f69b14a392
Madalina Serban <80857412+madalinaserban@users.noreply.github.com>
2022-05-23 16:32:31 +0300

    2

--- WpfApp1/ViewModels/WithAccountViewModel.cs
+++ WpfApp1/ViewModels/WithAccountViewModel.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WpfApp1.Services;
+using WpfApp1.View;
 
 namespace WpfApp1.ViewModels
 {
     class WithAccountViewModel
     {
+        private ICommand backCommand;
+        public ICommand BackCommand
+        {
+            get
+            {
+                if (backCommand == null)
+                {
+                    backCommand = new RelayCommand(BackMethod);
+                }
+                return backCommand;
+            }
+        }
+        private void BackMethod(object param)
+        {
+            StartWindow startWindow = new StartWindow();
+            App.Current.MainWindow.Close();
+            App.Current.MainWindow = startWindow;
+            startWindow.Show();
+        }
     }
 }
